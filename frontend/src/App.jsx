d68adbeb91fee0e9ef5f21e871285216160c2f08
Joyee Chakraborty <joyee.chakraborty@cqumail.com>
2026-09-23 14:25:23 +1000

    Add Interview Preparation page and interview service (WBS 6.7)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,75 +1,51 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes } from "react-router";
 
-import ProtectedRoute from './components/auth/ProtectedRoute'
-import StudentLayout from './layouts/StudentLayout'
-import CareerRecommendationsPage from './pages/CareerRecommendationsPage'
-import ExploreCareersPage from './pages/ExploreCareersPage'
-import CareerRoadmapPage from './pages/CareerRoadmapPage'
-import ForgotPasswordPage from './pages/ForgotPasswordPage'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import LearningResourcesPage from './pages/LearningResourcesPage'
-import NotFoundPage from './pages/NotFoundPage'
-import RegisterPage from './pages/RegisterPage'
-import ResetPasswordPage from './pages/ResetPasswordPage'
-import SkillGapAnalysisPage from './pages/SkillGapAnalysisPage'
-import StudentProfilePage from './pages/StudentProfilePage'
-
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import StudentLayout from "./layouts/StudentLayout";
+import CareerRecommendationsPage from "./pages/CareerRecommendationsPage";
+import ExploreCareersPage from "./pages/ExploreCareersPage";
+import CareerRoadmapPage from "./pages/CareerRoadmapPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import LearningResourcesPage from "./pages/LearningResourcesPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RegisterPage from "./pages/RegisterPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import SkillGapAnalysisPage from "./pages/SkillGapAnalysisPage";
+import StudentProfilePage from "./pages/StudentProfilePage";
+import InterviewPreparationPage from "./pages/InterviewPreparationPage";
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<HomePage />}
-      />
+      <Route path="/" element={<HomePage />} />
 
-      <Route
-        path="/login"
-        element={<LoginPage />}
-      />
+      <Route path="/login" element={<LoginPage />} />
 
-      <Route
-        path="/register"
-        element={<RegisterPage />}
-      />
+      <Route path="/register" element={<RegisterPage />} />
 
-      <Route
-        path="/forgot-password"
-        element={<ForgotPasswordPage />}
-      />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
-      <Route
-        path="/password/reset/confirm/"
-        element={<ResetPasswordPage />}
-      />
+      <Route path="/password/reset/confirm/" element={<ResetPasswordPage />} />
 
       <Route element={<ProtectedRoute />}>
         <Route element={<StudentLayout />}>
-          <Route
-            path="/profile"
-            element={<StudentProfilePage />}
-          />
+          <Route path="/profile" element={<StudentProfilePage />} />
 
           <Route
             path="/career-recommendations"
             element={<CareerRecommendationsPage />}
           />
 
-          <Route
-            path="/explore-careers"
-            element={<ExploreCareersPage />}
-          />
+          <Route path="/explore-careers" element={<ExploreCareersPage />} />
 
           <Route
             path="/skill-gap-analysis"
             element={<SkillGapAnalysisPage />}
           />
 
-          <Route
-            path="/career-roadmap"
-            element={<CareerRoadmapPage />}
-          />
+          <Route path="/career-roadmap" element={<CareerRoadmapPage />} />
 
           <Route
             path="/learning-resources"
@@ -78,13 +54,13 @@ function App() {
         </Route>
       </Route>
 
+      <Route path="*" element={<NotFoundPage />} />
       <Route
-        path="*"
-        element={<NotFoundPage />}
+        path="/interview-preparation"
+        element={<InterviewPreparationPage />}
       />
     </Routes>
-  )
+  );
 }
 
-
-export default App
+export default App;
